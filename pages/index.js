896cc2183ae605cc4c7477b17915c6a30c5322b4
IOLevi <evan@education.com>
2020-09-04 22:50:22 -0700

    update to two column layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Header from "../components/Header";
 import Topnav from "../components/Topnav";
-import {Accordion, Card} from "react-bootstrap";
 import styles from '../components/styles/Index.module.css';
-import MainContainer from "../components/MainContainer";
 import CollapseCard from "../components/CollapseCard";
 import Purple from "../components/Purple";
 
@@ -12,12 +10,21 @@ export default function Home() {
             <Header/>
             <Topnav/>
 
-            <div className={styles.flexContainer}>
-                <Purple/>
-                <CollapseCard name={'COVID'}/>
+            <div className={styles.row}>
+                <div className={styles.column}>
+                    <Purple/>
+                    <CollapseCard name={'COVID'}/>
+                    <CollapseCard name={'Closed streets'}/>
+
+
+                </div>
+                <div className={styles.column}>
+                    <CollapseCard name={'Hospitalizations'}/>
+                    <CollapseCard name={'DingoDong'}/>
+                    <CollapseCard name={'Closed streets'}/>
+                </div>
+
             </div>
-
-
         </>
     )
 }
